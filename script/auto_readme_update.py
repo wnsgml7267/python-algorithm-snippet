cf7a751607bad84addc10085df3107217fa892d6
wnsgml7267 <dlwns7267@naver.com>
2022-10-13 11:46:37 +0900

    Add script

---
+++ script/auto_readme_update.py
@@ -0,0 +1,28 @@
+from glob import glob
+
+def convert(text):
+    text = [
+        word.replace(word[0], word[0].upper(), 1)
+        for word in text.split('_')
+    ]
+    return " ".join(text)
+
+def make_readme():
+    files = [(x[4:-4], x) for x in glob('src/**/*.py', recursive=True)]
+    info = dict()
+    for file in files:
+        dep = file[0].split('/')
+        if dep[0] not in info:
+            info[dep[0]] = list()
+        info[dep[0]].append((dep[1], file[1]))
+
+    print("# Algorithm Snippet\n")
+    for key, value in info.items():
+        print(f"## {convert(key)}")
+        for x, y in value:
+            print(f"- [{convert(x)}]({y})")
+        print()
+
+
+if __name__ == "__main__":
+    make_readme()
