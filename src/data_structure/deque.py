747405fe043d765f457ab438e474dd3a8a01583e
wnsgml7267 <dlwns7267@naver.com>
2022-10-19 01:50:23 +0900

    Fix deque

--- src/data_structure/deque.py
+++ src/data_structure/deque.py
@@ -1,3 +1,13 @@
+#덱 : 큐와 스택을 포함
 from collections import deque
 q = deque()
-q.append((1,2))
+q.append(1)
+q.append(2)
+q.append(3)
+q.append(4)
+
+q.popleft() # 맨 앞 꺼내기
+q.pop() # 맨 뒤 꺼내기
+print(q)
+
+# 결과 : deque([2,3])
