74c0150b4a5f5b4f51a625f84cd8d828d4a7ce26
wnsgml7267 <dlwns7267@naver.com>
2022-10-19 01:48:39 +0900

    Fix queue

--- src/data_structure/queue.py
+++ src/data_structure/queue.py
@@ -1,5 +1,9 @@
-#큐는 deque 라이브러리를 가져와서 구현함
+#큐는 deque 라이브러리를 가져와서 구현함 맨 앞 인덱스를 뺌(FIFO)
 from collections import deque
 q = deque()
 q.append(1)
+q.append(2)
+q.append(3)
 q.popleft()
+print(q)
+# 결과 : deque([2, 3])
