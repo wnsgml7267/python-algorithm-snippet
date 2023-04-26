0d5be698a071edb820580176776e441db7a258ee
JunHee Lee <82314940+wnsgml7267@users.noreply.github.com>
2023-04-27 03:12:17 +0900

    Update queue.py

--- src/data_structure/queue.py
+++ src/data_structure/queue.py
@@ -1,4 +1,7 @@
-#큐는 deque 라이브러리를 가져와서 구현함 맨 앞 인덱스를 뺌(FIFO)
+# 큐는 deque 라이브러리를 가져와서 구현함 맨 앞 인덱스를 뺌(FIFO)
+# pop() : 맨 뒤 뺌
+# appendleft(0) : 맨 앞 추가
+# 정렬 불가
 from collections import deque
 q = deque()
 q.append(1)
